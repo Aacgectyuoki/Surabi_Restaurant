27466bb5fe1983f49f5586b658d7f70e324ae29a
aacgectyuoki <maxdell-thibodeau@hotmail.com>
2025-02-28 18:33:08 -0500

    SwaggerUI

---
+++ src/main/java/com/example/Surabi/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+//package com.example.Surabi.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authz -> authz
+//                        .anyRequest().authenticated())  // Ensure correct method call syntax
+//                .formLogin(withDefaults())  // Default login form
+//                .httpBasic(withDefaults()); // Enable basic authentication
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
